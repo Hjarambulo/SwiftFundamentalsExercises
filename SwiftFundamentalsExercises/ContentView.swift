35b098be15809927e78c20aea4335469ba9b446f
Humberto Arambulo <humbertoarambulo@MacBook-Air-de-Camila.local>
2024-11-09 16:35:34 -0300

    Gitignore agregado

--- SwiftFundamentalsExercises/ContentView.swift
+++ SwiftFundamentalsExercises/ContentView.swift
@@ -5,17 +5,6 @@
 //  Created by Humberto Arambulo on 09/11/2024.
 //
 
-//Ejercicios
-
-//    1.    Variables y constantes: Crea una variable para tu nombre, una constante para tu edad, y luego imprime un mensaje que combine ambos.
-//    2.    Tipos y Operadores básicos:
-//    •    Declara dos variables, x e y, asigna valores, y calcula su suma, resta, multiplicación y división. Luego imprime el resultado de cada operación.
-//    3.    Condicionales:
-//    •    Escribe un programa que determine si una persona es mayor de edad (18 o más) o no. Usa una variable para almacenar la edad.
-//    4.    Bucles:
-//    •    Crea un bucle for que imprima los números del 1 al 10.
-//    •    Haz un while que sume los números del 1 al 5 y luego imprima el total.
-
 import SwiftUI
 
 struct ContentView: View {
